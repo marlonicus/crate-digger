dde8c5ef09fccd9fde64a26cf8c75d982fe3b80d
Marlon Huber-Smith <marlon.huber-smith@akqa.com>
2018-11-13 22:35:12 +0100

    Add animation/loading buffer for 3D scene

--- templates/login.js
+++ templates/login.js
@@ -20,13 +20,28 @@ const LoginTemplate = () => (
           cursor: pointer;
         }
 
-        button:hover {
+        button:hover,
+        button:active,
+        button:focus {
           color: grey;
+        }
+
+        footer {
+          position: absolute;
+          bottom: 20px;
+          color: #777;
+        }
+
+        a {
+          color: #333;
         }
       `}
     </style>
-
     <button onClick={spotify.login}>go dig.</button>
+
+    <footer>
+      made by <a href="https://marlonic.us">marlonicus</a>
+    </footer>
   </main>
 );
 
--- templates/play.js
+++ templates/play.js
@@ -2,6 +2,7 @@ import { Scene } from "aframe-react";
 import { map, pathOr, bind } from "ramda";
 import spotify from "../utils/spotify";
 import Room from "../components/room";
+import Loader from "../components/loader";
 import Crate from "../components/crate";
 import Records from "../components/records";
 import Camera from "../components/camera";
@@ -20,7 +21,8 @@ class PlayTemplate extends React.Component {
 
     this.state = {
       viewingRecordIndex: false,
-      openRecordIndex: false
+      openRecordIndex: false,
+      readyState: 0
     };
   }
 
@@ -32,8 +34,16 @@ class PlayTemplate extends React.Component {
     const userData = await spotify.getTopArtists();
 
     this.setState({
-      userData
+      userData,
+      readyState: 1
     });
+
+    // Nasty, but just a little buffer for the scene to render and add a nice fadeout
+    setTimeout(() => {
+      this.setState({
+        readyState: 2
+      });
+    }, 700);
   };
 
   getNumberOfRecords() {
@@ -66,31 +76,54 @@ class PlayTemplate extends React.Component {
 
   render() {
     return (
-      <Scene
-        vr-mode-ui={{ enabled: true }}
-        cursor="rayOrigin: mouse; fuse: false"
-        raycaster="objects: .clickable"
-      >
-        <Room
-          isMusicPlaying={this.state.viewingRecordIndex !== false}
-          bpm={this.state.tempo}
-        />
+      <>
+        {this.state.readyState < 2 && (
+          <>
+            <style jsx>{`
+              .cover {
+                background: white;
+                width: 100vw;
+                height: 100vh;
+                position: absolute;
+                top: 0;
+                left: 0;
+                z-index: 2;
+                opacity: 1;
+                transition: opacity linear 200ms 500ms;
+                opacity: ${1 - this.state.readyState};
+              }
+            `}</style>
+            <div className="cover">
+              <Loader />
+            </div>
+          </>
+        )}
+        <Scene
+          vr-mode-ui={{ enabled: true }}
+          cursor="rayOrigin: mouse; fuse: false"
+          raycaster="objects: .clickable"
+        >
+          <Room
+            isMusicPlaying={this.state.viewingRecordIndex !== false}
+            bpm={this.state.tempo}
+          />
 
-        <Crate>
-          {this.state.userData && (
-            <Records
-              viewingRecordIndex={this.state.viewingRecordIndex}
-              openRecordIndex={this.state.openRecordIndex}
-              onRecordSelect={bind(this.viewRecord, this)}
-              onRecordClose={bind(this.closeRecord, this)}
-              onRecordOpen={bind(this.openRecord, this)}
-              tracks={map(getRecordFromTrackItem, this.state.userData.items)}
-            />
-          )}
-        </Crate>
+          <Crate>
+            {this.state.userData && (
+              <Records
+                viewingRecordIndex={this.state.viewingRecordIndex}
+                openRecordIndex={this.state.openRecordIndex}
+                onRecordSelect={bind(this.viewRecord, this)}
+                onRecordClose={bind(this.closeRecord, this)}
+                onRecordOpen={bind(this.openRecord, this)}
+                tracks={map(getRecordFromTrackItem, this.state.userData.items)}
+              />
+            )}
+          </Crate>
 
-        <Camera />
-      </Scene>
+          <Camera />
+        </Scene>
+      </>
     );
   }
 }
