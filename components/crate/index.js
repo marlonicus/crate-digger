d931be4a375a23c7ebb0c623179aa1a659c494e8
Marlon Huber-Smith <marlonhubersmith@gmail.com>
2018-11-29 21:01:41 +0100

    Clean up models folder

--- components/crate/index.js
+++ components/crate/index.js
@@ -3,7 +3,7 @@ import { Entity } from "aframe-react";
 
 export default ({ children, index }) => (
   <Entity
-    obj-model="obj:/static/models/untitled.obj"
+    obj-model="obj:/static/models/crate.obj"
     scale="1 1 1"
     position={`${(index % 4) * 2.3} -2.5 ${index < 4 ? "0" : "7"}`}
     rotation={`0 ${index < 4 ? "90" : "270"} 0`}
